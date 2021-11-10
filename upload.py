6ebf1733ecb17837b7ce83623752b40c2454a579
ChrisK197 <cskalish@gmail.com>
2021-11-10 11:53:03 -0500

    General cleanup (comments, spacing, printing)

--- upload.py
+++ upload.py
@@ -7,17 +7,13 @@ import base64
 import argparse
 import time
 import warnings
-
-import termcolor
 import urllib3.exceptions
-
-
-# Not using termcolor right now, did not get it to work in terminal
-from termcolor import colored
+from termcolor import *
 import colorama
 
+
+# Create argparser to use in terminal
 def argparser():
-    # Create argparser to use in terminal
     parser = argparse.ArgumentParser(description='Uploads the extracted firmware files in ExtractedFirmware.csv to FACT for analysis. See https://github.com/fkie-cad/FACT_core')
     parser.add_argument('-n', '--n', default=-1, help="Amount of bytes from each file uploaded to FACT. Defaults to entire file (-1)")
     parser.add_argument('-p', '--p', default="C:\\Users\\ACI\\Desktop\\RouterFirmware", help="Base path to firmware folder")
@@ -26,15 +22,15 @@ def argparser():
     return parser.parse_args()
 
 
+# Server setup
 def server():
-    # Server setup
     args = argparser()
     r = requests.get(os.path.join("{}/rest/status".format(args.s)), verify=False)
     return json.loads(r.text)
 
 
+# Setup plugins
 def plugins():
-    # Setup plugins
     server_status = server()
     available_plugins = server_status['plugins'].keys()
     used_plugins = ['cpu_architecture', 'software_components']
@@ -45,6 +41,7 @@ def plugins():
     return used_plugins
 
 
+# Run main analysis
 def main():
     warnings.filterwarnings('ignore', category=urllib3.exceptions.InsecureRequestWarning)
     args = argparser()
@@ -59,62 +56,62 @@ def main():
     with open(args.f, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            if row['vendor'] != '':     # Not needed anymore, since blank lines were removed later
-                # Fill in basic info
-                row['device_class'] = 'router'
-                row['device_part'] = 'complete'
-                row['requested_analysis_systems'] = used_plugins
-                row['tags'] = ""
-                if len(row['release_date']) == 0:
-                    row['release_date'] = "1970-01-01"     # Placeholder date
+            # Fill in basic info
+            row['device_class'] = 'router'
+            row['device_part'] = 'complete'
+            row['requested_analysis_systems'] = used_plugins
+            row['tags'] = ""
+            if len(row['release_date']) == 0:
+                row['release_date'] = "1970-01-01"     # Placeholder date
 
-                # Turning binary files to base64 strings
-                file_name = os.path.join("{}\\{}\\{}\\{}".format(args.p, row['vendor'], row['file_path'], row['file_name']))
-                file = open(file_name, mode='rb').read(int(args.n))
-                b64binary = base64.b64encode(file)
-                row['binary'] = b64binary.decode('ascii')
+            # Turning binary files to base64 strings
+            file_name = os.path.join("{}\\{}\\{}\\{}".format(args.p, row['vendor'], row['file_path'], row['file_name']))
+            file = open(file_name, mode='rb').read(int(args.n))
+            b64binary = base64.b64encode(file)
+            row['binary'] = b64binary.decode('ascii')
 
-                # File path was just a hardcoded column in my table for reference, not used in FACT
-                del row['file_path']
+            # File path was just a hardcoded column in my table for reference, not used in FACT
+            del row['file_path']
 
-                # Upload to FACT
-                r = requests.put(os.path.join("{}/rest/firmware".format(args.s)), json=row, verify=False)
-                response_data = json.loads(r.text)
+            # Upload to FACT
+            r = requests.put(os.path.join("{}/rest/firmware".format(args.s)), json=row, verify=False)
+            response_data = json.loads(r.text)
 
-                if "status" in response_data:
-                    status = response_data['status']
-                else:
-                    status = 1
-                    print(r.text)
+            if "status" in response_data:
+                status = response_data['status']
+            else:
+                status = 1
+                print(r.text)
 
-                if status == 0:
-                    print(colored("Uploaded {} file(s)".format(count), "cyan"))
-                    count += 1
-                else:
-                    print(colored("Failed to upload {} file".format(row["file_name"]), "red"))
-                    break
+            if status == 0:
+                print(colored("Uploaded {} file(s)".format(count), "cyan"))
+                count += 1
+            else:
+                print(colored("Failed to upload {} file".format(row["file_name"]), "red"))
+                break
 
+            # Wait for files to join the analysis queue, so that there are no more than 2 items being analysed at once
+            unpackingStatus = requests.get(os.path.join("{}/rest/status".format(args.s)), verify=False)
+            unpackingStatusJ = json.loads(unpackingStatus.text)
+            qcount = 0
+            while response_data["uid"] not in unpackingStatusJ["system_status"]["backend"]["analysis"]["current_analyses"]:
+                if qcount % 30 == 0:
+                    print("Joining queue\t|\tTime elapsed: {} seconds".format(qcount))
+                time.sleep(1)
                 unpackingStatus = requests.get(os.path.join("{}/rest/status".format(args.s)), verify=False)
                 unpackingStatusJ = json.loads(unpackingStatus.text)
-                qcount = 0
-                while response_data["uid"] not in unpackingStatusJ["system_status"]["backend"]["analysis"]["current_analyses"]:
-                    if qcount % 30 == 0:
-                        print("Joining queue\tTime elapsed: {} seconds".format(qcount))
-                    time.sleep(1)
-                    unpackingStatus = requests.get(os.path.join("{}/rest/status".format(args.s)), verify=False)
-                    unpackingStatusJ = json.loads(unpackingStatus.text)
-                    qcount += 1
-                print(colored("Joined queue", "yellow"))
-                print(colored("Total queue length: {}".format(len(unpackingStatusJ["system_status"]["backend"]["analysis"]["current_analyses"])), "yellow"))
-                qcount = 0
-                while len(unpackingStatusJ["system_status"]["backend"]["analysis"]["current_analyses"]) >= 2:
-                    if qcount % 30 == 0:
-                        print("Waiting for queue to process\tTime elapsed: {} seconds".format(qcount))
-                    time.sleep(1)
-                    unpackingStatus = requests.get(os.path.join("{}/rest/status".format(args.s)), verify=False)
-                    unpackingStatusJ = json.loads(unpackingStatus.text)
-                    qcount += 1
-        print(colored("Finished uploading all files", "green"))
+                qcount += 1
+            print(colored("Joined queue", "yellow"))
+            print(colored("Total queue length: {}".format(len(unpackingStatusJ["system_status"]["backend"]["analysis"]["current_analyses"])), "yellow"))
+            qcount = 0
+            while len(unpackingStatusJ["system_status"]["backend"]["analysis"]["current_analyses"]) >= 2:
+                if qcount % 30 == 0:
+                    print("Waiting for queue to process\t|\tTime elapsed: {} seconds".format(qcount))
+                time.sleep(1)
+                unpackingStatus = requests.get(os.path.join("{}/rest/status".format(args.s)), verify=False)
+                unpackingStatusJ = json.loads(unpackingStatus.text)
+                qcount += 1
+    print(colored("Finished uploading all files", "green"))
 
 
 if __name__ == '__main__':
