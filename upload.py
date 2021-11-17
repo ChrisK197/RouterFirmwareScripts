f8771ada8095534baa10da92d16bfe0f18392f4b
ChrisK197 <cskalish@gmail.com>
2021-11-17 12:11:36 -0500

    Fixed pep8, changed os.path to pathlib and eliminated path hardcoding, added UID check before uploading files and moved queue waiting to a new function, added file descriptions in regular print statements, fixed slight formatting error in ExtractedFirmware.csv

--- upload.py
+++ upload.py
@@ -1,6 +1,5 @@
 import csv
 import json
-import os
 import sys
 import requests
 import base64
@@ -8,25 +7,36 @@ import argparse
 import time
 import warnings
 import urllib3.exceptions
+from pathlib import Path, PureWindowsPath
+import hashlib
 from termcolor import *
 import colorama
 
 
 # Create argparser to use in terminal
 def argparser():
-    parser = argparse.ArgumentParser(description='Uploads the extracted firmware files in ExtractedFirmware.csv to FACT for analysis. See https://github.com/fkie-cad/FACT_core')
-    parser.add_argument('-n', '--num', default=-1, help="Amount of bytes from each file uploaded to FACT. Defaults to entire file (-1)")
-    parser.add_argument('-p', '--path', default="C:\\Users\\ACI\\Desktop\\RouterFirmware", help="Base path to firmware folder")
-    parser.add_argument('-s', '--server', default="https://192.168.253.129", help="Address of FACT server")
-    parser.add_argument('-f', '--file', default="ExtractedFirmware.csv", help="Name of csv file to be read")
-    parser.add_argument('-c', '--concurrent', default=2, type=int, help="Number of concurrent firmware extractions. Defaults to 2")
-    parser.add_argument('-d', '--disable', action="store_true", help="Disables limit on concurrent firmware analysis. Recommended for small files")
+    parser = argparse.ArgumentParser(
+        description='Uploads the extracted firmware files in ExtractedFirmware.csv to FACT for analysis. '
+        'See https://github.com/fkie-cad/FACT_core')
+    parser.add_argument('-n', '--num', default=-1,
+                        help="Amount of bytes from each file uploaded to FACT. Defaults to entire file (-1)")
+    parser.add_argument('-p', '--path', default="C:\\Users\\ACI\\Desktop\\RouterFirmware",
+                        help="Base path to firmware folder")
+    parser.add_argument('-s', '--server', default="https://192.168.253.129",
+                        help="Address of FACT server")
+    parser.add_argument('-f', '--file', default="ExtractedFirmware.csv",
+                        help="Name of csv file to be read")
+    parser.add_argument('-c', '--concurrent', default=2, type=int,
+                        help="Number of concurrent firmware extractions. Defaults to 2")
+    parser.add_argument('-d', '--disable', action="store_true",
+                        help="Disables limit on concurrent firmware analysis. Recommended for small files")
     return parser.parse_args()
 
 
 # Server setup
 def server(server_url):
     status = None
+    result = ""
     while status is None:
         r = requests.get("{}/rest/status".format(server_url), verify=False)
         result = json.loads(r.text)
@@ -46,29 +56,38 @@ def plugins(server_url):
     return used_plugins
 
 
-# Hardcoded method to get file path, only works on paths with at most 3 backslashes
-def get_file_name(row, args):
-    slashes = 0
-    for i in row['file_path']:
-        if i == "\\":
-            slashes += 1
-    if slashes == 0:
-        return os.path.join(args.path, row['vendor'], row['file_path'], row['file_name'])
-    elif slashes == 1:
-        fp1, fp2 = os.path.split(row['file_path'])
-        return os.path.join(args.path, row['vendor'], fp1, fp2, row['file_name'])
-    elif slashes == 2:
-        temp, fp3 = os.path.split(row['file_path'])
-        fp1, fp2 = os.path.split(temp)
-        return os.path.join(args.path, row['vendor'], fp1, fp2, fp3, row['file_name'])
-    elif slashes == 3:
-        temp, fp4 = os.path.split(row['file_path'])
-        temp2, fp3 = os.path.split(temp)
-        fp1, fp2 = os.path.split(temp2)
-        return os.path.join(args.path, row['vendor'], fp1, fp2, fp3, fp4, row['file_name'])
+# Taken from FACT_Core API
+def create_uid(input_data: bytes) -> str:
+    '''
+    generate a UID (unique identifier) SHA256_SIZE for a byte string containing data (e.g. a binary)
+    :param input_data: the data to generate the UID for
+    :return: a string containing the UID
+    '''
+    hash_value = hashlib.sha256(input_data).hexdigest()
+    if isinstance(input_data, bytes):
+        size = len(input_data)
+    if isinstance(input_data, str):
+        size = len(input_data.encode('utf-8'))
     else:
-        print(colored("File path longer than hardcoded values. This will be incompatible with anything besides Windows", "red"))
-        return "{}\\{}\\{}\\{}".format(args.path, row['vendor'], row['file_path'], row['file_name'])
+        size = len(bytes(input_data))
+    return '{}_{}'.format(hash_value, size)
+
+
+# Loops until a spot opens in the queue, based on the allowed concurrency
+def queueloop():
+    args = argparser()
+    server_status = server(args.server)
+    queue = server_status["system_status"]["backend"]["analysis"]["current_analyses"]
+    timer = 0
+
+    print(colored("Total queue length: {}".format(len(queue)), "yellow"))
+    while len(queue) >= args.concurrent:
+        if timer % 30 == 0:
+            print("Waiting for queue to process\t|\tTime elapsed: {} seconds".format(timer))
+        time.sleep(1)
+        server_status = server(args.server)
+        queue = server_status["system_status"]["backend"]["analysis"]["current_analyses"]
+        timer += 1
 
 
 # Run main analysis
@@ -90,9 +109,30 @@ def main():
             if len(row['release_date']) == 0:
                 row['release_date'] = "1970-01-01"     # Placeholder date
 
+            # Open binary file
+            file_name = PureWindowsPath("{}\\{}\\{}\\{}".format(
+                args.path, row['vendor'], row['file_path'], row['file_name']))
+            correct_path = Path(file_name)
+            file = open(correct_path, mode='rb').read(int(args.num))
+
+            # Checking if file is already being analyzed
+            uid = create_uid(file)
+            u = requests.get("{}/rest/firmware/{}".format(args.server, uid), verify=False)
+            u_text = json.loads(u.text)
+            u_status = u_text['status']
+            if u_status == 0:
+                print(colored("Already uploaded file {}: ".format(count), "cyan") +
+                      colored("{} {} v.{} ({}), \"{}\"".format(
+                          row['vendor'], row['device_name'], row['version'], row['release_date'], row['file_name']),
+                          "magenta"))
+                server_status = server(args.server)
+                queue = server_status["system_status"]["backend"]["analysis"]["current_analyses"]
+                if count >= args.concurrent:
+                    queueloop()
+                count += 1
+                continue
+
             # Turning binary files to base64 strings
-            file_name = get_file_name(row, args)
-            file = open(file_name, mode='rb').read(int(args.num))
             b64binary = base64.b64encode(file)
             row['binary'] = b64binary.decode('ascii')
 
@@ -106,32 +146,31 @@ def main():
             status = response_data.get('status', 1)
 
             if status == 0:
-                print(colored("Uploaded {} file(s)".format(count), "cyan"))
+                print(colored("Uploaded file {}: ".format(count), "cyan") +
+                      colored("\"{}\"".format(row['file_name']), "magenta"))
                 count += 1
             else:
                 print(r.text)
-                print(colored("Failed to upload {} file".format(row["file_name"]), "red"))
+                print(colored("Failed to upload file {}: ".format(row["file_name"]), "red") +
+                      colored("{} {} v.{} ({}), \"{}\"".format(
+                          row['vendor'], row['device_name'], row['version'], row['release_date'], row['file_name']),
+                          "magenta"))
                 break
 
             if not args.disable:
-                # Wait for files to join the analysis queue, so that there are no more than 2 items being analysed at once
+                # Wait for files to join analysis queue, so that there are no more than 2 items being analysed at once
                 server_status = server(args.server)
                 timer = 0
-                while response_data["uid"] not in server_status["system_status"]["backend"]["analysis"]["current_analyses"]:
+                queue = server_status["system_status"]["backend"]["analysis"]["current_analyses"]
+                while response_data["uid"] not in queue:
                     if timer % 30 == 0:
                         print("Joining queue\t|\tTime elapsed: {} seconds".format(timer))
                     time.sleep(1)
                     server_status = server(args.server)
+                    queue = server_status["system_status"]["backend"]["analysis"]["current_analyses"]
                     timer += 1
                 print(colored("Joined queue", "yellow"))
-                print(colored("Total queue length: {}".format(len(server_status["system_status"]["backend"]["analysis"]["current_analyses"])), "yellow"))
-                timer = 0
-                while len(server_status["system_status"]["backend"]["analysis"]["current_analyses"]) >= args.concurrent:
-                    if timer % 30 == 0:
-                        print("Waiting for queue to process\t|\tTime elapsed: {} seconds".format(timer))
-                    time.sleep(1)
-                    server_status = server(args.server)
-                    timer += 1
+                queueloop()
     print(colored("Finished uploading all files", "green"))
 
 
