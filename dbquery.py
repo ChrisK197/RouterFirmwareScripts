3f88ca4edbdfe4657c91513df95a0954ee673a95
ChrisK197 <cskalish@gmail.com>
2021-12-15 14:07:38 -0500

    software_lookup prints device names

--- dbquery.py
+++ dbquery.py
@@ -18,7 +18,7 @@ def argparser():
                         help="Looks up all of the firmwares associated with a software component")
     parser.add_argument('-c', '--count', action="store_true",
                         help="Counts the number of CVEs associated with each file that has at least one CVE")
-    parser.add_argument('-m', '--missing', action="store_true",
+    parser.add_argument('-n', '--none', action="store_true",
                         help="Counts all of the files without CVEs associated")
     parser.add_argument('-e', '--example', default=None,
                         help="Find example of software with CVE associated.")
@@ -43,22 +43,28 @@ def connect():
     # Get a database
     db = client.fact_main
 
-    # Get a collection
-    return db.file_objects
+    # Return the database
+    return db
 
 
-def software_lookup(file_objects, filename):
-    components = []
+def software_lookup(file_objects, filename, firmwares):
+    firmware_ids = []
     query = re.compile(".*(?i){}.*".format(filename))
     for i in file_objects.find({"file_name": query}):
         if 'cve_lookup' in i['processed_analysis'] and 'cve_results' in i['processed_analysis']['cve_lookup'] and len(
                 i['processed_analysis']['cve_lookup']['cve_results']) > 0:
             for j in i['parent_firmware_uids']:
-                if j not in components:
-                    components.append(j)
-    print("Total number of firmwares with software component \"" + filename + "\": " + str(len(components)))
-    # for i in components:
-    #     print(i)
+                if j not in firmware_ids:
+                    firmware_ids.append(j)
+    print("Total number of firmwares with software component \"" + filename + "\": " + str(len(firmware_ids)))
+    results = []
+    for i in firmware_ids:
+        query2 = {"_id": i}
+        data = firmwares.find_one(query2)
+        results.append("{} {} {}".format(data["vendor"], data["device_name"], data["version"]))
+    results.sort()
+    for i in results:
+        print(i)
 
 
 def count_cve(file_objects):
@@ -107,7 +113,7 @@ def alt_count_cve(file_objects):
     unique = set()
     for i in data:
         for j in i['processed_analysis']['software_components']['summary']:
-            unique.add(j.split(' ')[0])
+            unique.add(j.strip().rsplit(' ', 1)[0])
     print(unique)
     print("alt0 -- total number with CVEs {}".format(len(unique)))
     return unique
@@ -116,7 +122,7 @@ def alt_count_cve(file_objects):
 def alt2_count_cve(file_objects):
     field = "processed_analysis.cve_lookup.summary"
     data = file_objects.distinct(field)
-    unique = set(i.strip().split(' ')[0] for i in data)
+    unique = set(i.strip().rsplit(' ', 1)[0] for i in data)
     print(unique)
     print("alt2 -- total number with CVEs {}, unique {}".format(len(data), len(unique)))
     return unique
@@ -126,7 +132,7 @@ def alt3_count_cve(file_objects):
     field = "processed_analysis.software_components.summary"
     query = {"processed_analysis.cve_lookup.summary": {"$not": {"$size": 0}}}
     data = file_objects.distinct(field, query=query)
-    unique = set(i.strip().split(' ')[0] for i in data)
+    unique = set(i.strip().rsplit(' ', 1)[0] for i in data)
     print(unique)
     print("alt3 -- total number with CVEs {}, unique {}".format(len(data), len(unique)))
     return unique
@@ -134,22 +140,22 @@ def alt3_count_cve(file_objects):
 
 def no_cve(file_objects):
     components = []
-    for i in file_objects.find():
+    for i in file_objects:
         if 'cve_lookup' not in i['processed_analysis'] or 'cve_results' not in i['processed_analysis']['cve_lookup'] \
                 or len(i['processed_analysis']['cve_lookup']['cve_results']) == 0:
             if i['file_name'] not in components:
                 components.append(i['file_name'])
     print("Total number of software components without CVEs associated: " + str(len(components)))
     # pprint(components)
-    # for i in components:
-    # print(i)
+    for i in components:
+        print(i)
 
 
 def alt_no_cve(file_objects):
     field = "processed_analysis.software_components.summary"
     query = {"processed_analysis.cve_lookup.summary": {"$size": 0}}
     data = file_objects.distinct(field, query=query)
-    unique = set(i.strip().split(' ')[0] for i in data)
+    unique = set(i.strip().rsplit(' ', 1)[0] for i in data)
     print(data)
     print("alt -- total number without CVEs {}, unique {}".format(len(data), len(unique)))
     return unique
@@ -165,9 +171,11 @@ def print_example_file(file_objects, software):
 def main():
     warnings.filterwarnings('ignore', category=DeprecationWarning)
     args = argparser()
-    file_objects = connect()
+    db = connect()
+    file_objects = db.file_objects
+    firmwares = db.firmwares
     if args.lookup != 'null':
-        software_lookup(file_objects, args.lookup)
+        software_lookup(file_objects, args.lookup, firmwares)
     if args.count:
         # alt3_count_cve(file_objects)
         count_cve(file_objects)
@@ -187,7 +195,7 @@ def main():
                 m.add(i)
         print(m)
         '''
-    if args.missing:
+    if args.none:
         alt_no_cve(file_objects)
         # no_cve(file_objects)
     if args.example is not None:
